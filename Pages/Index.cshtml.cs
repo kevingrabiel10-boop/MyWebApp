8f98140a3d832d09a9af0671b0168373dae930cc
Kevin Grabiel Jurado <kevingrabiel10@gmail.com>
2025-12-10 19:28:31 -0500

    Actualización de mi web

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,8 +1,13 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-public class IndexModel : PageModel
+public class ProductoModel : PageModel
 {
+    public string Nombre { get; set; }
+    public decimal Precio { get; set; }
+
     public void OnGet()
     {
+        Nombre = "Consolador Turbo 3000";
+        Precio = 79.90m;
     }
 }
